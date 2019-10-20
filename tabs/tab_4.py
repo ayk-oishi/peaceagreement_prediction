b9ec442244b6a896f4982a24fda7841da48ba69f
Ayaka Oishi <ayakaoishi@Ayaka-no-MacBook-Air.local>
2019-10-20 11:33:17 -0400

    Imadechanges

--- tabs/tab_4.py
+++ tabs/tab_4.py
@@ -5,38 +5,38 @@ import pandas as pd
 
 filepath='resources/final_probs.csv'
 df=pd.read_csv(filepath)
-names=df['Name'].values
-index=df['Name'].index.values
+names=df['Agreement'].values
+index=df['Agreement'].index.values
 nameslist = list(zip(index, names))
 
 tab_4_layout = html.Div([
-    html.H3('Would you survive the Titanic?'),
+    html.H3('Is this peaceagreement successful?'),
     html.Div([
         html.Div('Select:', className='one column'),
         # Title,
         html.Div([
-            html.Div('Siblings and Spouses'),
+            html.Div('SymbolicReparation'),
             dcc.Dropdown(
-                id='family_dropdown',
-                options=[{'label': i, 'value': i} for i in range(0,9)],
-                value='2',
+                id='SymbolicReparation_dropdown',
+                options=[{'label': i, 'value': i} for i in range(0,2)],
+                value='0',
                 ),
         ],className='three columns'),
         html.Div([
-            html.Div('Age'),
+            html.Div('Amnesty'),
             dcc.Dropdown(
-                id='age_dropdown',
-                options=[{'label': i, 'value': i} for i in range(1,81)],
-                value='25',
+                id='Amnesty_dropdown',
+                options=[{'label': i, 'value': i} for i in range(0,4)],
+                value='0',
 
                 ),
         ],className='three columns'),
         html.Div([
-            html.Div('Cabin Class'),
+            html.Div('Ceasefire'),
             dcc.Dropdown(
-                id='cabin_dropdown',
-                options=[{'label': i, 'value': i} for i in ['First', 'Second', 'Third']],
-                value='First',
+                id='Ceasefire_dropdown',
+                options=[{'label': i, 'value': i} for i in range(0,4)],
+                value='0',
                 ),
         ],className='four columns'),
         html.Div('     ', className='one column')
@@ -47,37 +47,37 @@ tab_4_layout = html.Div([
     html.Br(),
     html.Br(),
 
-    html.Div([
-        html.Div('Select:', className='one column'),
-        html.Div([
-            html.Div('Title'),
-            dcc.RadioItems(
-                id='title_radio',
-                options=[{'label': i, 'value': i} for i in ['Mr.', 'Miss', 'Mrs.', 'VIP']],
-                value='None',
-                ),
-        ],className='three columns'),
-        html.Div([
-            html.Div('Sex'),
-            dcc.RadioItems(
-                id='sex_radio',
-                options=[{'label': i, 'value': i} for i in ['Male', 'Female']],
-                value='None',
-                ),
-        ],className='three columns'),
-        html.Div([
-            html.Div('Port of Embarkation'),
-            dcc.RadioItems(
-                id='port_radio',
-                options=[{'label': i, 'value': i} for i in ['Cherbourg', 'Queenstown', 'Southampton']],
-                value='None',
-                ),
-        ],className='five columns'),
-    ],className='twelve columns'),
-    html.Br(),
-    html.Br(),
-    html.Br(),
-    html.Br(),
+#    html.Div([
+#        html.Div('Select:', className='one column'),
+#        html.Div([
+#            html.Div('Title'),
+#            dcc.RadioItems(
+#                id='title_radio',
+#                options=[{'label': i, 'value': i} for i in ['Mr.', 'Miss', 'Mrs.', 'VIP']],
+#                value='None',
+#                ),
+#        ],className='three columns'),
+#        html.Div([
+#            html.Div('Sex'),
+#            dcc.RadioItems(
+#                id='sex_radio',
+#                options=[{'label': i, 'value': i} for i in ['Male', 'Female']],
+#                value='None',
+#                ),
+#        ],className='three columns'),
+#        html.Div([
+#            html.Div('Port of Embarkation'),
+#            dcc.RadioItems(
+#                id='port_radio',
+#                options=[{'label': i, 'value': i} for i in ['Cherbourg', 'Queenstown', 'Southampton']],
+#                value='None',
+#                ),
+#        ],className='five columns'),
+#    ],className='twelve columns'),
+#    html.Br(),
+#    html.Br(),
+#    html.Br(),
+#    html.Br(),
     # Output results
     html.Div([
         html.Div(id='user-inputs-box', style={'text-align':'center','fontSize':18}),
